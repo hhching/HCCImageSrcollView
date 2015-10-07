4f11006b62911cc7be4448f7b476566627b1cc9c
apple <apple@appledeMacBook-Pro.local>
2015-10-07 17:02:31 +0600

    初始化项目

---
+++ HCCImageSrcollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCCImageSrcollView
+//
+//  Created by apple on 15/10/7.
+//  Copyright (c) 2015年 com.h.limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HCCImageSrcollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCCImageSrcollView
+//
+//  Created by apple on 15/10/7.
+//  Copyright (c) 2015年 com.h.limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
